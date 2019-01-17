f50f2b00e933b63a9b396d14ddc6ecf243941381
berialyue <berialyue@gmail.com>
2019-01-17 22:22:27 +0800

    add function

---
+++ demo04.js
@@ -0,0 +1,11 @@
+"use strict";
+function zhengXing() {
+    var yangzia = '小沈阳';
+    {
+        var yangzib = '马德华';
+        console.log(yangzib);
+    }
+    console.log(yangzia);
+    console.log(yangzib);
+}
+zhengXing();
---
+++ demo04.ts
@@ -0,0 +1,14 @@
+function zhengXing():void {
+  let yangzia:string = '小沈阳'
+  
+  {
+    let yangzib:string = '马德华'
+    console.log(yangzib)
+  }
+
+  console.log(yangzia)
+  console.log(yangzib)
+  
+}
+
+zhengXing()
