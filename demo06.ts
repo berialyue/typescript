787f6a6a5bee6d54c48c3431c4f178397ed837a2
berialyue <berialyue@gmail.com>
2019-01-19 22:10:54 +0800

    add String

---
+++ demo06.ts
@@ -0,0 +1,18 @@
+let berial:string = 'berialyue'
+let yueduming:String = new String('berial')
+console.log(berial)
+console.log(yueduming)
+
+console.log(berial.length)
+console.log(yueduming.length)
+
+let something:string = "清早起来打开窗，心情美美的，我要出去找小姐姐，心情美美的。"
+let xiaoJieJie:string = "小姐姐"
+
+console.log(something.indexOf(xiaoJieJie))
+console.log(something.lastIndexOf(xiaoJieJie))
+
+console.log(something.substring(8))
+console.log(something.substring(8,14))
+
+console.log(something.replace(xiaoJieJie, '小哥哥'))
