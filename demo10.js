364815c75e2140f818f48bb846e77c4e34511a19
berialyue <berialyue@gmail.com>
2019-01-23 23:00:23 +0800

    add public protected private

---
+++ demo10.js
@@ -0,0 +1,21 @@
+"use strict";
+var XiaoJieJie2 = /** @class */ (function () {
+    function XiaoJieJie2(sex, name, age) {
+        this.sex = sex;
+        this.name = name;
+        this.age = age;
+    }
+    XiaoJieJie2.prototype.sayHello = function () {
+        console.log('berial');
+    };
+    XiaoJieJie2.prototype.sayLove = function () {
+        console.log('i love you');
+    };
+    return XiaoJieJie2;
+}());
+var jiejie2 = new XiaoJieJie2('女', '热巴', 22);
+console.log(jiejie2.sex);
+console.log(jiejie2.name);
+console.log(jiejie2.age);
+jiejie2.sayHello();
+jiejie2.sayLove();
