9f3bb06d84bf8f3c59e7f767e649e01ca3844a3c
berialyue <berialyue@gmail.com>
2019-02-10 23:46:20 +0800

    内置对象

--- typescript.ts
+++ typescript.ts
@@ -197,3 +197,16 @@ function getLength(something:string | number):number {
 declare var jQuery:(selector:string) => any
 
 jQuery('#foo')
+
+// 内置对象
+let b:Boolean = new Boolean(1)
+let e:Error = new Error('error')
+let d:Date = new Date()
+let r:RegExp = /[a-z]/
+
+let body:HTMLElement = document.body
+let allDiv:NodeList = document.querySelectorAll('div')
+document.addEventListener('click', function(e:MouseEvent){
+  // do something
+})
+
