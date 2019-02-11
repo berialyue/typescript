00f4c49064160b9e0392ca0fe90479dbe114f45a
berialyue <berialyue@gmail.com>
2019-02-11 12:55:19 +0800

    类型别名

--- typescript.ts
+++ typescript.ts
@@ -210,3 +210,14 @@ document.addEventListener('click', function(e:MouseEvent){
   // do something
 })
 
+// 类型别名
+type Name = string
+type NameResolver = () => string
+type NameOrResolver = Name | NameResolver
+function getName(n:NameOrResolver):Name {
+  if(typeof n === 'string') {
+    return n
+  } else {
+    return n()
+  }
+}
